8356b02da00c7dfa84dce6b802190669b0730181
Nickson Ho <nicksonho@Nicksons-MacBook-Pro.local>
2026-05-05 17:36:37 +0800

    Edit documentation format

--- chessEngine.py
+++ chessEngine.py
@@ -4,11 +4,13 @@ It will also be responsible for determining the valid moves at the current state
 It will also keep a move log.
 """
 class GameState():
+    """
+    The board is an 8x8 2D List, each element of the list has 2 characters.
+    The first character represents the color of the piece, 'b' or 'w'. 
+    The second character represents the type of the piece. 'K', 'Q', 'R', 'B', 'N', 'P'
+    "--" represents an empty space with no piece.    
+    """
     def __init__(self):
-        # The board is an 8x8 2D List, each element of the list has 2 characters.
-        # The first character represents the color of the piece, 'b' or 'w'. 
-        # The second character represents the type of the piece. 'K', 'Q', 'R', 'B', 'N', 'P'
-        # "--" represents an empty space with no piece.
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
